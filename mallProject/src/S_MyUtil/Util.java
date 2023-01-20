655850e595944e89fe72c4dc2adbc6b0b853a406
yeonmi <dus9728@gmail.com>
2023-01-20 14:29:21 +0900

    update util.java
    public String getValue(String msg) 추가

--- mallProject/src/S_MyUtil/Util.java
+++ mallProject/src/S_MyUtil/Util.java
@@ -4,4 +4,10 @@ import java.util.Scanner;
 
 public class Util {
 	static public  Scanner scan = new Scanner(System.in);
+	
+	public String getValue(String msg) {
+		System.out.println(msg+">>");
+		String value = scan.next();
+		return value;
+	}
 }
